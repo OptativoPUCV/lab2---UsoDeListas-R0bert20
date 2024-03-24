9c4f118868742a9e9b61af4d5662262e6baff4c3
Your Name <you@example.com>
2024-03-24 00:01:59 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -122,40 +122,6 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 */
 
 int parentesisBalanceados(char *cadena) {
-  char* elemento = top(cadena);
-
-
-  while(elemento != NULL){
-    int cont = 0;
-    if(elemento == '('){
-      pop(cadena);
-      elemento = top(cadena);
-      if(elemento != ')'){
-        cont++;
-        pop(cadena);
-        elemento = top(cadena);
-      } 
-
-    }else if(elemento == '{'){
-      pop(cadena);
-      elemento = top(cadena);
-      if (elemento != '}'){
-        cont++;
-        pop(cadena);
-        elemento = top(cadena);
-      } 
-
-    } else if(elemento == '['){
-      pop(cadena);
-      elemento = top(cadena);
-      if(elemento != ']'){
-        cont++;
-        pop(cadena);
-        elemento = top(cadena);
-      } 
-    }
-
-    if(cont > 0) return 0;
-  }
-  return 1;
+  List* lista = create_list();
+  
 }
